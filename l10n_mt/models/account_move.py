dc67c5828f3797eabac396ca335c2fbc2b49e7bc
imedC <imed08@hotmail.fr>
2020-12-28 15:27:26 +0100

    add vat_line field

--- l10n_mt/models/account_move.py
+++ l10n_mt/models/account_move.py
@@ -9,18 +9,20 @@ class AccountMoveLine(models.Model):
     net_value = fields.Monetary(string='Net Value',compute='get_net_value',currency_field='company_currency_id',store=True)
     gross_value = fields.Monetary(string='Gross Value',compute='get_gross_value',currency_field='company_currency_id',store=True)
     account_group = fields.Char(compute='get_account_group_name',string='Account Group',store=True)
+    vat_line_id = fields.Many2one('account.tax', string='VAT',store=True)
 
-    @api.depends('tax_base_amount')
+    @api.depends('tax_base_amount','tax_audit')
     def get_tax_value(self):
 
         for record in self:
             record.tax_value = 0
-            for tag in record.tag_ids:
-                tag_amount = (tag.tax_negate and -1 or 1) * (record.move_id.is_inbound() and -1 or 1) * record.balance
-                if tag_amount:
-                    record.tax_value = tag_amount
+            if record.account_group == 'OO7':
+                for tag in record.tag_ids:
+                    tag_amount = (tag.tax_negate and -1 or 1) * (record.move_id.is_inbound() and -1 or 1) * record.balance
+                    if tag_amount:
+                        record.tax_value = tag_amount
 
-    @api.depends('tax_base_amount')
+    @api.depends('tax_base_amount','tax_audit')
     def get_net_value(self):
 
         for record in self:
@@ -30,13 +32,13 @@ class AccountMoveLine(models.Model):
                     record.net_value = record.tax_base_amount * -1
                 else:
                     record.net_value = record.tax_base_amount
+            else:
+                record.net_value = record.move_id.amount_untaxed
 
-    @api.depends('tax_base_amount')
+    @api.depends('tax_base_amount','tax_audit')
     def get_gross_value(self):
         for record in self:
-            record.gross_value = 0
-            if record.tax_value and record.net_value:
-                record.gross_value = record.net_value + record.tax_value
+            record.gross_value = record.net_value + record.tax_value
 
     @api.depends('account_id')
     def get_account_group_name(self):
@@ -45,3 +47,19 @@ class AccountMoveLine(models.Model):
             record.account_group = ''
             if record.account_id:
                 record.account_group = record.account_id.group_id.code_prefix
+
+class AccountMove(models.Model):
+    _inherit='account.move'
+
+
+    @api.onchange('line_ids','invoice_line_ids')
+    def onchange_vat_line_id(self):
+        for line in self.line_ids:
+            line.vat_line_id = False
+            if line.tax_ids:
+                for taxe in line.tax_ids:
+                    if taxe.amount == 0.0:
+                        line.vat_line_id = taxe.ids[0]
+
+            if line.tax_line_id:
+                line.vat_line_id = line.tax_line_id
