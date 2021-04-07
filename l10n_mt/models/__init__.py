01becbac7020daf5aebec39116908aedc5cc7d81
imedC <imed08@hotmail.fr>
2021-04-07 19:55:06 +0100

    change vat report

--- l10n_mt/models/__init__.py
+++ l10n_mt/models/__init__.py
@@ -2,3 +2,4 @@
 from . import chart_template
 from . import account_move
 from . import account_followup_report
+from . import account_vat_report
--- l10n_mt/models/account_move.py
+++ l10n_mt/models/account_move.py
@@ -1,11 +1,5 @@
 # -*- coding: utf-8 -*-
 from odoo import models, fields, _, api
-import logging
-import json
-from odoo.tools import config
-import lxml.html
-
-_logger = logging.getLogger(__name__)
 
 
 class AccountAccount(models.Model):
@@ -19,27 +13,15 @@ class AccountMove(models.Model):
 
     @api.model
     def cron_compute_field(self):
+
         for line in self.env['account.move'].search([]).line_ids:
             line.vat_line_id = False
-            if line.tax_ids:
-                for taxe in line.tax_ids:
-                    if taxe.amount == 0.0:
-                        line.vat_line_id = taxe.ids[0]
-
-            if line.tax_line_id:
-                line.vat_line_id = line.tax_line_id
-
-    @api.onchange('line_ids', 'invoice_line_ids')
-    def onchange_vat_line_id(self):
-        for line in self.line_ids:
+        for line in self.env['account.move'].search([]).invoice_line_ids:
             line.vat_line_id = False
             if line.tax_ids:
-                for taxe in line.tax_ids:
-                    if taxe.amount == 0.0:
-                        line.vat_line_id = taxe.ids[0]
+                line.vat_line_id = line.tax_ids.ids[0]
+                line.tax_value = line.price_total - line.price_subtotal
 
-            if line.tax_line_id:
-                line.vat_line_id = line.tax_line_id
 
 
 class AccountMoveLine(models.Model):
@@ -47,35 +29,28 @@ class AccountMoveLine(models.Model):
 
     tax_value = fields.Monetary(string='Tax Value', compute='get_tax_value', currency_field='company_currency_id',
                                 store=True)
-    net_value = fields.Monetary(string='Net Value', compute='get_net_value', currency_field='company_currency_id',
+    net_value = fields.Monetary(string='Net Value', compute='get_tax_value', currency_field='company_currency_id',
                                 store=True)
     gross_value = fields.Monetary(string='Gross Value', compute='get_gross_value', currency_field='company_currency_id',
                                   store=True)
     account_group = fields.Char(compute='get_account_group_name', string='Account Group', store=True)
-    vat_line_id = fields.Many2one('account.tax', string='VAT', store=True)
+    vat_line_id = fields.Many2one('account.tax', string='VAT', compute='get_vat_line', store=True)
 
-    @api.depends('tax_base_amount', 'tax_audit')
+
+    @api.depends('tax_ids')
+    def get_vat_line(self):
+        for rec in self:
+            if rec.tax_ids:
+                rec.vat_line_id = rec.tax_ids.ids[0]
+            else:
+                rec.vat_line_id = False
+
+    @api.depends('price_subtotal')
     def get_tax_value(self):
 
         for record in self:
-            record.tax_value = 0
-            record.tax_value = record.move_id.amount_tax
-
-    @api.depends('tax_base_amount', 'tax_audit')
-    def get_net_value(self):
-
-        for record in self:
-            record.net_value = 0
-            if record.tax_base_amount:
-                if record.move_id.move_type in ['out_refund', 'in_refund']:
-                    record.net_value = record.tax_base_amount * -1
-                else:
-                    record.net_value = record.tax_base_amount
-            else:
-                if record.move_id.move_type in ['out_refund', 'in_refund']:
-                    record.net_value = record.price_subtotal * -1
-                else:
-                    record.net_value = record.price_subtotal
+            record.net_value = record.price_subtotal
+            record.tax_value = record.price_total - record.price_subtotal
 
     @api.depends('net_value', 'tax_value')
     def get_gross_value(self):
@@ -89,147 +64,3 @@ class AccountMoveLine(models.Model):
             record.account_group = ''
             if record.account_id:
                 record.account_group = record.account_id.group_id.code_prefix_start
-
-    @api.model
-    def print_pdf(self, options):
-
-        return {
-            'type': 'ir_actions_vat_report_download',
-            'data': {'model': 'account.move.line',
-                     'options': json.dumps(options),
-                     'output_format': 'pdf',
-                     }
-        }
-
-    def get_pdf(self, options, minimal_layout=True, **kwargs):
-
-        if not config['test_enable']:
-            self = self.with_context(commit_assetsbundle=True)
-
-        base_url = self.env['ir.config_parameter'].sudo().get_param('report.url') or self.env[
-            'ir.config_parameter'].sudo().get_param('web.base.url')
-
-        line = []
-
-        line_ids = self.env['account.move.line'].search(options,
-                                                        order="vat_line_id asc, date desc, move_id asc")
-        actual_vat_code = False
-        dict_total_value_per_tax = {}
-        dict_total_value = {'net_value':0, 'tax_value':0, 'gross_value': 0}
-
-        for line_id in line_ids:
-            dict_total_value['net_value'] += line_id.net_value
-            dict_total_value['tax_value'] += line_id.tax_value
-            dict_total_value['gross_value'] += line_id.gross_value
-
-        line.append(['TOTAL', '', '', '%.2f' % dict_total_value['net_value'], '%.2f' % dict_total_value['tax_value'], '%.2f' % dict_total_value['gross_value']])
-
-        for line_id in line_ids:
-            if (str(str(line_id.vat_line_id.id)) + '_net' and str(line_id.vat_line_id.id) + '_tax' and str(line_id.vat_line_id.id) + '_gross') in dict_total_value_per_tax:
-                dict_total_value_per_tax[str(line_id.vat_line_id.id) + '_net'] +=  line_id.net_value
-                dict_total_value_per_tax[str(line_id.vat_line_id.id) + '_tax'] +=  line_id.tax_value
-                dict_total_value_per_tax[str(line_id.vat_line_id.id) + '_gross'] +=  line_id.gross_value
-            else:
-                dict_total_value_per_tax[str(line_id.vat_line_id.id) + '_net'] = line_id.net_value
-                dict_total_value_per_tax[str(line_id.vat_line_id.id) + '_tax'] = line_id.tax_value
-                dict_total_value_per_tax[str(line_id.vat_line_id.id) + '_gross'] = line_id.gross_value
-
-        for line_id in line_ids:
-            if line_id.vat_line_id.id != actual_vat_code:
-                line.append(
-                    [line_id.vat_line_id.name,'',
-                     '', '%.2f' % dict_total_value_per_tax[str(line_id.vat_line_id.id) + '_net'], '%.2f' % dict_total_value_per_tax[str(line_id.vat_line_id.id) + '_tax'], '%.2f' % dict_total_value_per_tax[str(line_id.vat_line_id.id) + '_gross']])
-
-            actual_vat_code = line_id.vat_line_id.id
-
-            line.append(
-                [line_id.vat_line_id.name if line_id.vat_line_id.id != actual_vat_code else '', line_id.date,
-                 line_id.move_id.name, line_id.net_value, line_id.tax_value, line_id.gross_value])
-
-        info_domain = {}
-        date_start = False
-        date_end = False
-        for op in options:
-            if type(op) is list:
-                if op[0] == 'move_id.state' and op[2] == 'posted':
-                    info_domain['state'] = 'posted'
-
-                if op[0] == 'date' and op[1] == '>=':
-                    if not date_start:
-                        date_start = op[2]
-                    else:
-                        if date_start >= op[2]:
-                            date_start = op[2]
-
-                    info_domain['date_start'] = date_start
-
-                if op[0] == 'date' and op[1] == '<=':
-                    if not date_end:
-                        date_end = op[2]
-                    else:
-                        if date_end <= op[2]:
-                            date_end = op[2]
-
-                    info_domain['date_end'] = date_end
-
-        rcontext = {
-            'mode': 'print',
-            'base_url': base_url,
-            'company': self.env.company,
-            'lines': {'columns_header': ['', 'Date', 'Journal', 'Net Value', 'Tax Value', 'Gross Value'],
-                      'lines': line, 'other_info': info_domain
-                      }}
-
-        body = self.env['ir.ui.view']._render_template(
-            "l10n_mt.vat_template",
-            values=dict(rcontext),
-        )
-
-        if minimal_layout:
-            header = ''
-            footer = self.env['ir.actions.report']._render_template("web.internal_layout", values=rcontext)
-            spec_paperformat_args = {'data-report-margin-top': 10, 'data-report-header-spacing': 10}
-            footer = self.env['ir.actions.report']._render_template("web.minimal_layout",
-                                                                    values=dict(rcontext, subst=True, body=footer))
-        else:
-            rcontext.update({
-                'css': '',
-                'o': self.env.user,
-                'res_company': self.env.company,
-            })
-            header = self.env['ir.actions.report']._render_template("web.external_layout", values=rcontext)
-            header = header.decode('utf-8')  # Ensure that headers and footer are correctly encoded
-            spec_paperformat_args = {}
-            # Default header and footer in case the user customized web.external_layout and removed the header/footer
-            headers = header.encode()
-            footer = b''
-            # parse header as new header contains header, body and footer
-            try:
-                root = lxml.html.fromstring(header)
-                match_klass = "//div[contains(concat(' ', normalize-space(@class), ' '), ' {} ')]"
-
-                for node in root.xpath(match_klass.format('header')):
-                    headers = lxml.html.tostring(node)
-                    headers = self.env['ir.actions.report']._render_template("web.minimal_layout",
-                                                                             values=dict(rcontext, subst=True,
-                                                                                         body=headers))
-
-                for node in root.xpath(match_klass.format('footer')):
-                    footer = lxml.html.tostring(node)
-                    footer = self.env['ir.actions.report']._render_template("web.minimal_layout",
-                                                                            values=dict(rcontext, subst=True,
-                                                                                        body=footer))
-
-            except lxml.etree.XMLSyntaxError:
-                headers = header.encode()
-                footer = b''
-            header = headers
-
-        landscape = False
-
-        return self.env['ir.actions.report']._run_wkhtmltopdf(
-            [body],
-            header=header, footer=footer,
-            landscape=landscape,
-            specific_paperformat_args=spec_paperformat_args
-        )
