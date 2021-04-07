fd1cd40443b8e09a9daed02c4f8a1e2842379bce
imedC <imed08@hotmail.fr>
2021-04-07 11:37:56 +0100

    change followup report name

---
+++ 4sight_module/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': '4Sight - Module',
+    'version': '1.0',
+    'category': '',
+    'description': """
+    """,
+    'author': '4Sight Group',
+    'website': '',
+    'depends': [
+        'account',
+        'account_reports',
+        'account_followup',
+    ],
+    'data': [
+        'views/account_followups.xml',
+    ],
+    'demo': [
+    ],
+}
