fc17eb05574f32fdc76ca1431308bb3d2282c2e5
imedC <imed08@hotmail.fr>
2022-02-18 10:48:15 +0100

    set up fields in tree/form

--- 4sight_crm/__manifest__.py
+++ 4sight_crm/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': '4Sight - Module',
+    'name': '4Sight - Custom CRM',
     'version': '1.0',
     'category': '',
     'description': """
@@ -8,24 +8,12 @@
     'author': '4Sight Group',
     'website': '',
     'depends': [
-        'account',
-        'account_reports',
-        'account_followup',
-        'web',
-        'report_qweb_element_page_visibility',
-        'helpdesk',
-        'crm',
-        "mail"
+        'crm','website','website_form'
     ],
     'data': [
-        'views/account_followups.xml',
-        'views/sale_view.xml',
-        'report/external_layout_background.xml',
-        'views/view_helpdesk_form.xml',
-        'views/view_helpdesk_team.xml',
-        'views/view_crm_kanban.xml',
-        'views/view_account_inv_send.xml',
-        'views/view_partner_form.xml',
+        'data/website_data.xml',
+        'views/template_customer_data_retail.xml',
+        'views/customer_data.xml'
     ],
     'demo': [
     ],
--- 4sight_crm/models/__init__.py
+++ 4sight_crm/models/__init__.py
@@ -1,8 +1,2 @@
 # -*- coding: utf-8 -*-
-from . import account_followup_report
-from . import helpdesk_ticket
-from . import account_invoice_send
-from . import mail_thread
 from . import crm_lead
-from . import res_partner
-from . import mail_message
