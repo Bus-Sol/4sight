3a7db69df86cfb15ee1d253c78bd2507374e8985
frahman30 <frahman@i-datatech.com>
2023-11-23 14:24:18 +0500

    MODIFY TIMESHEET ENTRIES FIXES

--- modify_timesheet_entries/models/models.py
+++ modify_timesheet_entries/models/models.py
@@ -18,6 +18,9 @@ class AccountAnalyticLineInherits(models.Model):
                 str_unit_amount_before_float = split_unit_amount[0]
                 str_unit_amount_after_float = split_unit_amount[1]
 
+                if int(str_unit_amount_after_float) < 10:
+                    str_unit_amount_after_float = str_unit_amount_after_float + '0'
+
                 int_unit_amount_before_float = int(str_unit_amount_before_float)
                 int_unit_amount_after_float = int(str_unit_amount_after_float)
 
@@ -30,6 +33,17 @@ class AccountAnalyticLineInherits(models.Model):
                     elif 45 > int_unit_amount_after_float > 30:
                         int_unit_amount_after_float = 45
                     elif 60 > int_unit_amount_after_float > 45:
+                        int_unit_amount_before_float += 1
+                        int_unit_amount_after_float = 00
+
+                    print('INT UNIT AMOUNT AFTER FLOAT IS ::: ', int_unit_amount_after_float)
+                    str_unit_amount_to_save = str(str(int_unit_amount_before_float) + '.' + str(int_unit_amount_after_float))
+                    print('STR UNIT AMOUNT TO SAVE IS ::: ', str_unit_amount_to_save)
+                    print('FLOAT UNIT AMOUNT TO SAVE IS ::: ', float(str_unit_amount_to_save))
+                    self.env.cr.execute("""UPDATE account_analytic_line SET unit_amount=%s WHERE id=%s""", (float(str_unit_amount_to_save), rec.id))
+                else:
+                    print('NEEDS ROUNDING')
+                    if int_unit_amount_after_float == 60:
                         int_unit_amount_before_float += 1
                         int_unit_amount_after_float = 00
 
@@ -51,6 +65,9 @@ class AccountAnalyticLineInherits(models.Model):
                     str_unit_amount_before_float = split_unit_amount[0]
                     str_unit_amount_after_float = split_unit_amount[1]
 
+                    if int(str_unit_amount_after_float) < 10:
+                        str_unit_amount_after_float = str_unit_amount_after_float + '0'
+
                     int_unit_amount_before_float = int(str_unit_amount_before_float)
                     int_unit_amount_after_float = int(str_unit_amount_after_float)
 
@@ -63,6 +80,17 @@ class AccountAnalyticLineInherits(models.Model):
                         elif 45 > int_unit_amount_after_float > 30:
                             int_unit_amount_after_float = 45
                         elif 60 > int_unit_amount_after_float > 45:
+                            int_unit_amount_before_float += 1
+                            int_unit_amount_after_float = 00
+
+                        print('INT UNIT AMOUNT AFTER FLOAT IS ::: ', int_unit_amount_after_float)
+                        str_unit_amount_to_save = str(str(int_unit_amount_before_float) + '.' + str(int_unit_amount_after_float))
+                        print('STR UNIT AMOUNT TO SAVE IS ::: ', str_unit_amount_to_save)
+                        print('FLOAT UNIT AMOUNT TO SAVE IS ::: ', float(str_unit_amount_to_save))
+                        values['unit_amount'] = float(str_unit_amount_to_save)
+                    else:
+                        print('NEEDS ROUNDING')
+                        if int_unit_amount_after_float == 60:
                             int_unit_amount_before_float += 1
                             int_unit_amount_after_float = 00
 
