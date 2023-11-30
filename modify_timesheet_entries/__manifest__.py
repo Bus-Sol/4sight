e23b9db1a51f94496bc3ba19aaeecea99d9bf2bf
frahman30 <frahman@i-datatech.com>
2023-11-30 18:22:09 +0500

    MODIFY TIMESHEET ENTRIES FIXES

--- modify_timesheet_entries/__manifest__.py
+++ modify_timesheet_entries/__manifest__.py
@@ -3,9 +3,9 @@
     'name': "MODIFY TIMESHEET ENTRIES",
     'author': "Fazal Ur Rahman",
     'category': 'Extra Tools',
-    'version': '16.0.0.1',
+    'version': '14.0.0.1',
 
-    'depends': ['base', 'timesheet_grid'],
+    'depends': ['base', 'timesheet_grid', 'sale', 'purchase', 'product', 'account', 'odoo_timestead'],
 
     'data': [
         'views/views.xml',
--- modify_timesheet_entries/models/models.py
+++ modify_timesheet_entries/models/models.py
@@ -1,6 +1,123 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError
+
+
+class ClientJobSheetInherits(models.Model):
+    _inherit = 'client.jobsheet'
+
+    @api.model
+    def create(self, values):
+        x_unit_amount = values.get('hours')
+        x_str_unit_amount = str(x_unit_amount)
+        x_split_unit_amount = x_str_unit_amount.split('.', 1)
+        x_str_unit_amount_after_float = x_split_unit_amount[1]
+
+        if int(x_str_unit_amount_after_float) < 10 and '0' not in x_str_unit_amount_after_float:
+            x_str_unit_amount_after_float = x_str_unit_amount_after_float + '0'
+
+        x_int_unit_amount_after_float = int(x_str_unit_amount_after_float)
+        if x_int_unit_amount_after_float > 60:
+            raise ValidationError("Not Allowed To Enter Minutes Greater Then 60!")
+
+        res = super().create(values)
+        for rec in res:
+            if rec.partner_id.x_timesheets_rounding:
+                str_unit_amount = str(rec.hours)
+                split_unit_amount = str_unit_amount.split('.', 1)
+                print('ORIGINAL SPLIT UNIT AMOUNT IS ::: ', split_unit_amount)
+
+                str_unit_amount_before_float = split_unit_amount[0]
+                str_unit_amount_after_float = split_unit_amount[1]
+
+                if int(str_unit_amount_after_float) < 10 and '0' not in str_unit_amount_after_float:
+                    str_unit_amount_after_float = str_unit_amount_after_float + '0'
+
+                int_unit_amount_before_float = int(str_unit_amount_before_float)
+                int_unit_amount_after_float = int(str_unit_amount_after_float)
+
+                if (int_unit_amount_after_float % 15) != 0:
+                    print('NEEDS ROUNDING')
+                    if 15 > int_unit_amount_after_float > 0:
+                        int_unit_amount_after_float = 15
+                    elif 30 > int_unit_amount_after_float > 15:
+                        int_unit_amount_after_float = 30
+                    elif 45 > int_unit_amount_after_float > 30:
+                        int_unit_amount_after_float = 45
+                    elif 60 > int_unit_amount_after_float > 45:
+                        int_unit_amount_before_float += 1
+                        int_unit_amount_after_float = 00
+
+                    print('INT UNIT AMOUNT AFTER FLOAT IS ::: ', int_unit_amount_after_float)
+                    str_unit_amount_to_save = str(str(int_unit_amount_before_float) + '.' + str(int_unit_amount_after_float))
+                    print('STR UNIT AMOUNT TO SAVE IS ::: ', str_unit_amount_to_save)
+                    print('FLOAT UNIT AMOUNT TO SAVE IS ::: ', float(str_unit_amount_to_save))
+                    self.env.cr.execute("""UPDATE client_jobsheet SET hours=%s WHERE id=%s""", (float(str_unit_amount_to_save), rec.id))
+                else:
+                    print('NEEDS ROUNDING')
+                    if int_unit_amount_after_float == 60:
+                        int_unit_amount_before_float += 1
+                        int_unit_amount_after_float = 00
+
+                    print('INT UNIT AMOUNT AFTER FLOAT IS ::: ', int_unit_amount_after_float)
+                    str_unit_amount_to_save = str(str(int_unit_amount_before_float) + '.' + str(int_unit_amount_after_float))
+                    print('STR UNIT AMOUNT TO SAVE IS ::: ', str_unit_amount_to_save)
+                    print('FLOAT UNIT AMOUNT TO SAVE IS ::: ', float(str_unit_amount_to_save))
+                    self.env.cr.execute("""UPDATE client_jobsheet SET hours=%s WHERE id=%s""", (float(str_unit_amount_to_save), rec.id))
+        return res
+
+    def write(self, values):
+        if values.get('hours'):
+            for rec in self:
+                if rec.partner_id.x_timesheets_rounding:
+                    str_unit_amount = str(values.get('hours'))
+                    split_unit_amount = str_unit_amount.split('.', 1)
+                    print('ORIGINAL SPLIT UNIT AMOUNT IS ::: ', split_unit_amount)
+
+                    str_unit_amount_before_float = split_unit_amount[0]
+                    str_unit_amount_after_float = split_unit_amount[1]
+
+                    if int(str_unit_amount_after_float) < 10 and '0' not in str_unit_amount_after_float:
+                        str_unit_amount_after_float = str_unit_amount_after_float + '0'
+
+                    int_unit_amount_before_float = int(str_unit_amount_before_float)
+                    int_unit_amount_after_float = int(str_unit_amount_after_float)
+
+                    if int_unit_amount_after_float > 60:
+                        raise ValidationError("Not Allowed To Enter Minutes Greater Then 60!")
+
+                    if (int_unit_amount_after_float % 15) != 0:
+                        print('NEEDS ROUNDING')
+                        if 15 > int_unit_amount_after_float > 0:
+                            int_unit_amount_after_float = 15
+                        elif 30 > int_unit_amount_after_float > 15:
+                            int_unit_amount_after_float = 30
+                        elif 45 > int_unit_amount_after_float > 30:
+                            int_unit_amount_after_float = 45
+                        elif 60 > int_unit_amount_after_float > 45:
+                            int_unit_amount_before_float += 1
+                            int_unit_amount_after_float = 00
+
+                        print('INT UNIT AMOUNT AFTER FLOAT IS ::: ', int_unit_amount_after_float)
+                        str_unit_amount_to_save = str(str(int_unit_amount_before_float) + '.' + str(int_unit_amount_after_float))
+                        print('STR UNIT AMOUNT TO SAVE IS ::: ', str_unit_amount_to_save)
+                        print('FLOAT UNIT AMOUNT TO SAVE IS ::: ', float(str_unit_amount_to_save))
+                        values['hours'] = float(str_unit_amount_to_save)
+                    else:
+                        print('NEEDS ROUNDING')
+                        if int_unit_amount_after_float == 60:
+                            int_unit_amount_before_float += 1
+                            int_unit_amount_after_float = 00
+
+                        print('INT UNIT AMOUNT AFTER FLOAT IS ::: ', int_unit_amount_after_float)
+                        str_unit_amount_to_save = str(str(int_unit_amount_before_float) + '.' + str(int_unit_amount_after_float))
+                        print('STR UNIT AMOUNT TO SAVE IS ::: ', str_unit_amount_to_save)
+                        print('FLOAT UNIT AMOUNT TO SAVE IS ::: ', float(str_unit_amount_to_save))
+                        values['hours'] = float(str_unit_amount_to_save)
+
+        res = super().write(values)
+        return res
 
 
 class AccountAnalyticLineInherits(models.Model):
@@ -8,6 +125,18 @@ class AccountAnalyticLineInherits(models.Model):
 
     @api.model
     def create(self, values):
+        x_unit_amount = values.get('unit_amount')
+        x_str_unit_amount = str(x_unit_amount)
+        x_split_unit_amount = x_str_unit_amount.split('.', 1)
+        x_str_unit_amount_after_float = x_split_unit_amount[1]
+
+        if int(x_str_unit_amount_after_float) < 10 and '0' not in x_str_unit_amount_after_float:
+            x_str_unit_amount_after_float = x_str_unit_amount_after_float + '0'
+
+        x_int_unit_amount_after_float = int(x_str_unit_amount_after_float)
+        if x_int_unit_amount_after_float > 60:
+            raise ValidationError("Not Allowed To Enter Minutes Greater Then 60!")
+
         res = super().create(values)
         for rec in res:
             if rec.user_id.partner_id.x_timesheets_rounding:
@@ -70,6 +199,9 @@ class AccountAnalyticLineInherits(models.Model):
 
                     int_unit_amount_before_float = int(str_unit_amount_before_float)
                     int_unit_amount_after_float = int(str_unit_amount_after_float)
+
+                    if int_unit_amount_after_float > 60:
+                        raise ValidationError("Not Allowed To Enter Minutes Greater Then 60!")
 
                     if (int_unit_amount_after_float % 15) != 0:
                         print('NEEDS ROUNDING')
