ddb7092d8b72f844c214d50bb31601871fc671cb
sanjaygohel <gohelsanjayv@gmail.com>
2024-07-03 17:12:12 +0530

    [IMP]jobsheet changes which was changed by husam 4 months ago migrated

--- odoo_timestead/models/jobsheet_reports.py
+++ odoo_timestead/models/jobsheet_reports.py
@@ -18,35 +18,38 @@ class ReportJobsheetBalance(models.AbstractModel):
         lines = []
         context = self.env.context
         report = self.env['account.report']
-        query = """
-            SELECT
-                DISTINCT ON (partner.name, product.name, task.check_balance)
-                partner.name, product.name,
-                CASE WHEN (task.check_balance = 'balanced') THEN 'Balanced'
-                ELSE 'Unbalanced' END
-                FROM project_task task 
-                RIGHT JOIN res_partner partner ON partner.id = task.partner_id
-                LEFT JOIN product_template product ON product.id = task.related_service_id
-                WHERE partner.jobsheet_type = 'prepaid'
-                ORDER BY task.check_balance, product.name, partner.name;
-        """
         report_value = dict()
         prepaid_partners_with_services = self.env['res.partner'].search(
             [('jobsheet_type', '=', 'prepaid'), ('service_ids', '!=', False)], order='name')
         for partner in prepaid_partners_with_services:
-            for service in partner.service_ids:
-                task = self.env['project.task'].search([
-                    ('partner_id', '=', partner.id),
-                    ('related_service_id', '=', service.product_id.id)
-                ], limit=1, order='id desc')
-                if task:
-                    planned_hours = '{0:02.0f}:{1:02.0f}'.format(*divmod(task.planned_hours * 60, 60))
-                    effective_hours = '{0:02.0f}:{1:02.0f}'.format(*divmod(task.effective_hours * 60, 60))
-                    remaining_hours = '{0:02.0f}:{1:02.0f}'.format(*divmod(task.remaining_hours * 60, 60))
-                    report_value[('%s_%s') % (partner.id, service.product_id.id)] = [
-                        partner.name, service.product_id.name, planned_hours, effective_hours,
-                        remaining_hours, dict(task._fields['check_balance'].selection).get(task.check_balance)
-                    ]
+            for service in partner.service_ids:                
+                tks1 = self.env['project.task'].search(
+                    [('partner_id', '=', partner.id), 
+                     ('related_service_id', '=', service.product_id.id),
+                     ('sale_line_id','!=',False),
+                     ('sale_line_id.order_id.state','in',['sale','done'])],
+                    order='id desc')
+                tks = [task for task in tks1 if round(task.remaining_hours, 2) > 0]
+                planned = 0
+                effective = 0
+                remaining = 0
+                for tsk in tks:
+                    planned += tsk.planned_hours
+                    effective += tsk.effective_hours
+                    remaining += tsk.remaining_hours
+
+                if tks:
+                    planned_hours = '{0:02.0f}:{1:02.0f}'.format(*divmod(planned * 60, 60))
+                    effective_hours = '{0:02.0f}:{1:02.0f}'.format(*divmod(effective * 60, 60))
+                    remaining_hours = '{0:02.0f}:{1:02.0f}'.format(*divmod(remaining * 60, 60))
+                    report_value[('%s_%s') % (partner.id, service.product_id.id)] = [partner.name,
+                                                                                     service.product_id.name,
+                                                                                     planned_hours,
+                                                                                     effective_hours,
+                                                                                     remaining_hours,
+                                                                                     "Balanced"
+                                                                                     ]       
+                                                                                     
                 else:
                     report_value[('%s_%s') % (partner.id, service.product_id.id)] = [
                         partner.name, service.product_id.name, '00:00', '00:00', '00:00', 'Out of Balance']
