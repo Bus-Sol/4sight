46dc4aea4e456a8dd8a33110c86c53d438dfd85e
Mohamed Daoud <4sight-mdaoud@github.com>
2024-10-16 16:18:24 +0300

    fix revolut payment neutralize

--- revolut_payment/data/neutralize.sql
+++ revolut_payment/data/neutralize.sql
@@ -1,5 +1,5 @@
 -- disable revolut payment provider
 UPDATE payment_provider
    SET revolut_merchant_api_public = NULL,
-   revolut_merchant_api_public = NULL;
+   revolut_merchant_api_secret = NULL;
 
