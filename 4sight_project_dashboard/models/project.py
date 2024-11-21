505b3bdd839676a3d19110db57dea9f49b300d35
Mohamed Daoud <4sight-mdaoud@github.com>
2024-11-21 16:54:08 +0200

    update dashboard

--- 4sight_project_dashboard/models/project.py
+++ 4sight_project_dashboard/models/project.py
@@ -12,6 +12,10 @@ class Project(models.Model):
     effective_hours = fields.Float(string="Spent Hours", group_operator="sum", compute="get_project_hours")
     progress = fields.Float(string="Progress", group_operator="avg", compute="get_progress")
 
+    paid_hours = fields.Float(string="Client Paid Hours")
+    remaining_from_paid = fields.Float(string="Remaining hours from Client Paid",compute="get_project_hours")
+    paid_progress = fields.Float(string="Paid Progress", group_operator="avg", compute="get_progress")
+
     achievements = fields.Text(string="Achievements")
     next_deliverables = fields.Text(string="Next Deliverables")
     dependencies = fields.Text(string="Issues/Risks/Dependencies")
@@ -24,16 +28,18 @@ class Project(models.Model):
             rec.invoice_total = sum([i.amount_total for i in rec.sale_order_id.invoice_ids])
             rec.invoice_due = sum([i.amount_residual for i in rec.sale_order_id.invoice_ids])
 
-    @api.depends('task_ids.effective_hours')
+    @api.depends('task_ids.effective_hours','paid_hours')
     def get_project_hours(self):
         for rec in self:
             rec.effective_hours = sum([t.effective_hours for t in rec.task_ids])
             rec.tasks_allocated_hours = sum([t.allocated_hours for t in rec.task_ids])
             rec.tasks_remaining_hours = sum([t.allocated_hours - t.effective_hours for t in rec.task_ids])
+            rec.remaining_from_paid = rec.paid_hours - rec.effective_hours
 
 
-    @api.depends('allocated_hours', 'effective_hours')
+    @api.depends('allocated_hours', 'tasks_allocated_hours','paid_hours','remaining_from_paid')
     def get_progress(self):
         for rec in self:
-            rec.progress = (rec.effective_hours * 100 ) / rec.allocated_hours if rec.allocated_hours > 0 else 0
+            rec.progress = (rec.effective_hours * 100 ) / rec.tasks_allocated_hours if rec.tasks_allocated_hours > 0 else 0
+            rec.paid_progress = (rec.remaining_from_paid * 100 ) / rec.paid_hours if rec.paid_hours > 0 else 0
 
