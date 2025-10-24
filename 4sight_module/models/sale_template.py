f9df4a7f7618ad8e7e2a2b8d617d40dcc323358c
Mohamed Daoud <4sight-mdaoud@github.com>
2025-10-24 14:48:52 +0300

    update flow template

--- 4sight_module/models/sale_template.py
+++ 4sight_module/models/sale_template.py
@@ -7,3 +7,8 @@ class SaleOrderTemplate(models.Model):
     is_flow_template = fields.Boolean(string="Flow Template")
     flow_image = fields.Binary(string="Flow Logo")
     flow_details = fields.Text(string="Flow Company Details")
+    flow_email = fields.Char(string="Flow Email")
+    flow_website = fields.Char(string="Flow Website")
+    flow_phone = fields.Char(string="Flow Phone")
+    flow_vat = fields.Char(string="Flow VAT")
+
