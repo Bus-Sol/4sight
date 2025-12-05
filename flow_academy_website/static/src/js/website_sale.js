00f243aa68af9956555b831f19393ef290a274c9
Mohamed Daoud <4sight-mdaoud@github.com>
2025-12-05 03:20:23 +0200

    update address page

--- flow_academy_website/static/src/js/website_sale.js
+++ flow_academy_website/static/src/js/website_sale.js
@@ -8,40 +8,42 @@ WebsiteSale.include({
     /**
      * @override
      */
-    start() {
-        const def = this._super(...arguments);
-        if (document.getElementById("company_type")) {
-            this._onChangeCompanyType();
-        }
-        return def;
+    init() {
+        this._super(...arguments);
+
     },
 
-    //--------------------------------------------------------------------------
-    // Private
-    //--------------------------------------------------------------------------
-    /**
-     * Event click, hidden fields l10n_cl_activity_description
-     * if l10n_cl_sii_taxpayer_type is 'ticket'
-     *
-     * @private
-     */
+    start() {
+
+        this.contactTypeSelect = this.$('#company_type');
+        this.individualFields = this.$('.o_individual_field');
+        this.companyFields = this.$('.o_company_field');
+
+        console.log('contactTypeSelect', this.contactTypeSelect)
+        console.log('companyFields', this.companyFields)
+
+
+
+        // if (document.getElementById("company_type")) {
+        //     this._onChangeCompanyType();
+        // }
+        return this._super.apply(this, arguments);
+    },
+
+
 
 
      _onChangeCompanyType: function(ev) {
-        const selectedIdentificationType = ev.currentTarget.options[ev.currentTarget.selectedIndex].text
+        const companyType = document.querySelector('select[name="company_type"]');
+        const targetValue = ev.currentTarget.value
+        console.log('companyType', companyType)
+        console.log('targetValue', targetValue)
+        var is_company =  targetValue === 'company'
 
-        if (selectedIdentificationType === "NIT") {
-            this.obligationTypeBlock.classList.remove("d-none");
-            this.fiscalRegimenBlock.classList.remove("d-none");
-        } else {
-            this.obligationTypeBlock.classList.add("d-none");
-            this.fiscalRegimenBlock.classList.add("d-none");
-        }
+        this.individualFields.toggle(!is_company);
+        this.companyFields.toggle(is_company);
+
     },
 
-    _onChangeCompanyType() {
-        const typeDocumentEl = document.querySelector('input[name="l10n_cl_type_document"]');
-        const checked = typeDocumentEl.checked ? "none" : "flex";
-        document.getElementById("div_l10n_cl_additional_fields").style.display = checked;
-    },
+
 });
