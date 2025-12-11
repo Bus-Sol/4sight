b80e5c82731ad3ef5c0d2c747d582c07ccfd67c9
Mohamed Daoud <4sight-mdaoud@github.com>
2025-12-11 22:57:18 +0200

    update flow app

--- flow_academy_website/models/event.py
+++ flow_academy_website/models/event.py
@@ -5,3 +5,10 @@ class Event(models.Model):
     _inherit = "event.event"
 
     event_category_id = fields.Many2one(comodel_name="event.category", string="Event Category")
+
+    price = fields.Monetary(string="price",  currency_field='currency_id', compute="compute_price")
+
+    @api.depends('event_ticket_ids', 'event_ticket_ids.price')
+    def compute_price(self):
+        for rec in self:
+            rec.price = max(rec.event_ticket_ids.mapped('price')) if rec.event_ticket_ids else 0
