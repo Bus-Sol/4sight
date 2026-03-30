24f0e5af68d31dce9793ae2548fc8291d5dd05e7
Mohamed Daoud <4sight-mdaoud@github.com>
2026-03-30 17:30:28 +0200

    update project list

--- 4sight_project_dashboard/models/project.py
+++ 4sight_project_dashboard/models/project.py
@@ -31,18 +31,15 @@ class Project(models.Model):
         for rec in self:
             rec.is_account_manager = self.env.user.has_group('account.group_account_manager')
 
-
     def action_open_project(self):
         self.ensure_one()
-        action = self.env['ir.actions.act_window'].with_context(active_id=self.id)._for_xml_id('project.open_view_project_all')
+        action = self.env['ir.actions.act_window'].with_context(active_id=self.id)._for_xml_id(
+            '4sight_project_dashboard.project_project_action_view')
         action["views"] = [
             (self.env.ref("project.edit_project").id, "form")
         ]
-        action['res_id'] = self.id
-        context = action['context'].replace('active_id', str(self.id))
-        context = ast.literal_eval(context)
 
-        action['context'] = context
+        action['res_id'] = self.id
         return action
 
     @api.depends('sale_order_id','sale_order_id.invoice_ids')
