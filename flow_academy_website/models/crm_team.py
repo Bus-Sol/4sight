6eeb4113382b8b0b8e24831e928519b61d4bc85b
Mohamed Daoud <4sight-mdaoud@github.com>
2026-04-09 14:36:16 +0200

    update crm

--- flow_academy_website/models/crm_team.py
+++ flow_academy_website/models/crm_team.py
@@ -7,6 +7,4 @@ class CrmTeam(models.Model):
     sale_order_template_id = fields.Many2one(
         "sale.order.template",
         string="Quotation Template",
-        domain="['|', ('company_id', '=', False), ('company_id', '=', company_id)]",
-        check_company=True,
     )
